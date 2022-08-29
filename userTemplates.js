30764a9a59beb038fcbc31fd6d372314c3d17ca8
Rejone E Rasul Hridoy <Rejone.Rasul@brainstation-23.com>
2022-08-30 00:58:40 +0600

    added media query and js

---
+++ userTemplates.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '85246',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '85245',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '85863',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '82572',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    }
+];
